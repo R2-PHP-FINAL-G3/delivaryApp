070a423d13e75d1baa3fd91f59f9ee6b0b7dee58
elbayoumi <mohamedashrafelbayoumi.com>
2023-03-02 16:58:13 +0200

    v1.0.0

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,16 +10,17 @@ import { Router } from '@angular/router';
 export class LoginComponent {
   loginForm!:FormGroup;
   submitted = false;
+  userName: any;
 
   constructor(private fb:FormBuilder,private router:Router){}
 
   ngOnInit(): void
   {
     this.loginForm = this.fb.group({
-      userName:['',[Validators.required,Validators.email]],
+      userName:['',[Validators.required,Validators.min(4)]],
       passWord:['',[Validators.required,Validators.pattern(
-        // '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$' for email
-'^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$'
+        '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$'
+// '^[a-zA-Z0-9]([._-](?![._-])|[a-zA-Z0-9]){3,18}[a-zA-Z0-9]$' for email
       )]]
     });
   }
@@ -37,8 +38,10 @@ export class LoginComponent {
           localStorage.setItem("passWord",this.loginForm.get('passWord')?.value);
           this.clear();
           this.loginForm.disable();
-          this.router.navigate(['orders']);
+          this.router.navigate(['orders',this.userName]);
+          console.log(this.userName)
        }
+
   }
 
   clear()
