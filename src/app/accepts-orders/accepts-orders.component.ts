caa527d34de3acd8e8c66649e3290df76ba3f036
elbayoumi <mohamedashrafelbayoumi.com>
2023-03-02 11:59:02 +0200

    v1

--- src/app/accepts-orders/accepts-orders.component.ts
+++ src/app/accepts-orders/accepts-orders.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import {Order} from '../interfaces/order'
 import { AllOrdersService } from './services/all-orders.service';
+import {LocalStorageService} from '../services/local-storage.service';
 @Component({
   selector: 'app-accepts-orders',
   templateUrl: './accepts-orders.component.html',
@@ -10,10 +11,13 @@ import { AllOrdersService } from './services/all-orders.service';
 export class AcceptsOrdersComponent {
 
 
-constructor(private router: Router , private service:AllOrdersService,private activatedRoute :ActivatedRoute){}
+constructor(private router: Router , private service:AllOrdersService,private activatedRoute :ActivatedRoute , private localStorageService:LocalStorageService){}
 @Input() dataArrayForApi:Order[]=[
   {id:'1',title:"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops","price":109.95,"description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday","category":"men's clothing","image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg","rating":{"rate":3.9,"count":120}}
 ];
+@Input() dataApi:any[]=[
+];
+
 goTo(prop:any,i:any){
 
   this.router.navigate([prop,this.dataArrayForApi[i].id])
@@ -24,17 +28,21 @@ backTo(prop:any){
 getOrders(){
 this.service.getAllOrders().subscribe((res:any) => {
 this.dataArrayForApi=res
-console.log(res)
+const currentArrayOfData= this.dataArrayForApi.find(i => i.id==this.activatedRoute.snapshot.paramMap.get('id'))
+console.log(currentArrayOfData)
+if (this.dataApi.find(i => i.id==this.activatedRoute.snapshot.paramMap.get('id'))==currentArrayOfData)
+{}
+else
+this.dataApi.push(currentArrayOfData)
+// console.log(res)
 
 })
 }
-currentData(){
-console.log(this.activatedRoute.snapshot.paramMap.get('id'))
-const currentArrayOfData= this.dataArrayForApi.find(i => i.id==this.activatedRoute.snapshot.paramMap.get('id'))
-console.log(currentArrayOfData)
+setValue() {
+
+  this.localStorageService.setItem('dataApi', this.dataApi);
 }
 ngOnInit(): void {
 this.getOrders();
-this.currentData();
 }
 }
