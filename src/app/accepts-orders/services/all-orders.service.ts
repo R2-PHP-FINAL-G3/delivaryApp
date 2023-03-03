7f95c8300b46448b2a4cf3ae7bad788e3a1f4ecc
AtaAllA <100690634+AtaAllA@users.noreply.github.com>
2023-03-03 11:44:28 +0200

    v1.0.0 by AtaAlla

--- src/app/accepts-orders/services/all-orders.service.ts
+++ src/app/accepts-orders/services/all-orders.service.ts
@@ -4,14 +4,16 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
+
 export class AllOrdersService {
 
   constructor(private http: HttpClient) { }
 getAllOrders(){
-  return this.http.get('https://fakestoreapi.com/products')
-}
+  
+  return this.http.get('https://fakestoreapi.com/products') 
+} 
 postData(data: any){
   this.http.post('https://fakestoreapi.com/products', data)
 }
 
-}
+};                                                        
