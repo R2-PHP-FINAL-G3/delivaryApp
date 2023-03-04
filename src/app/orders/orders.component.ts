9ea6dcc7d4bb56dd7d7927649a21f10fd2db1b6b
elbayoumi <mohamedashrafelbayoumi.com>
2023-03-04 02:56:54 +0200

    v1.2.0

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -10,6 +10,7 @@ import { Order } from '../interfaces/order';
 })
 export class OrdersComponent implements OnInit  {
 constructor(private route: Router, private service:OrdersService){}
+laoding:boolean = false;
 @Input() dataArrayForApi:Order[]=[
   {id:'1',title:"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops","price":109.95,"description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday","category":"men's clothing","image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg","rating":{"rate":3.9,"count":120}}
 ];
@@ -36,7 +37,9 @@ else{
 }
 //
 getOrders(){
+  this.laoding=true
   this.service.getAllOrders().subscribe((res: any) =>{
+    this.laoding=false
 
     // console.log(res);
     this.dataArrayForApi=res
@@ -45,6 +48,7 @@ getOrders(){
 ,
 (error:any)=>{
   console.log( error.message);
+  this.laoding=false
 
 },
 // ()=>{
